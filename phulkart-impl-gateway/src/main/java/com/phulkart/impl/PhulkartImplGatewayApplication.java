611d10a328a4cd070ed2b6968a809c25893c958d
User <birajkhatiwoda@gmail.com>
2021-06-19 19:32:42 +0530

    initial commit

---
+++ phulkart-impl-gateway/src/main/java/com/phulkart/impl/PhulkartImplGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.phulkart.impl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhulkartImplGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhulkartImplGatewayApplication.class, args);
+	}
+
+}
---
+++ phulkart-impl-gateway/src/test/java/com/phulkart/impl/PhulkartImplGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phulkart.impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhulkartImplGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
